4789bc9c99bc787bdc1bc0f9669a04b88d328a5d
chengshantze <chengshantze@gmail.com>
2025-06-09 15:34:46 +0800

    新增地雷

---
+++ Assets/Script/bullet/ExplosionBullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionBullet : Bullet
+{
+    // Start is called before the first frame update
+    public int ExplosionTime;
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+    }
+    // Update is called once per frame
+    protected override void Update()
+    {
+        
+        
+    }
+}
